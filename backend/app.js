f11ce2755759e13ad172fd9f3cda2e0f66278ae2
Hasibul Huda <hasibulhuda1@gmail.com>
2020-01-01 18:14:05 +0600

    cors added

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const mysql = require('mysql')
+const cors = require('cors')
+const server = require('./routes/server')
 require('dotenv').config()
 
 
 
 const app = express()
+
+app.use(cors())
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(express.json())
 
 
 const connection = mysql.createConnection({
@@ -22,8 +30,11 @@ connection.connect(err=>{
     }
 })
 
+//Redirect root endpoint to server.js
+app.use('/', server)
+
 
 
 app.listen(3000, ()=>{
-    console.log('Server Running')
+    console.log('Server Running on 3000')
 })
